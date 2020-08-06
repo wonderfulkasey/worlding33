75643f0f3363b05dee079018e89fe473d9dace5a
wonderfulkasey <kaseyarnold15@gmail.com>
2020-08-06 11:09:08 -0400

    used devise to create plots worlds characters

---
+++ app/models/character.rb
@@ -0,0 +1,6 @@
+class Character < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :worlds
+  devise_for :plots
+  devise_for :characters
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
