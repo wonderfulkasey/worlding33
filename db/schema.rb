60125fd2581910acbba066962b0ae765a1bfdf9b
wonderfulkasey <kaseyarnold15@gmail.com>
2020-08-06 11:13:58 -0400

    db migrate done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_150115) do
+ActiveRecord::Schema.define(version: 2020_08_06_150846) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_characters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_characters_on_reset_password_token", unique: true
+  end
+
+  create_table "plots", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_plots_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_plots_on_reset_password_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +48,18 @@ ActiveRecord::Schema.define(version: 2020_08_06_150115) do
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "worlds", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_worlds_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_worlds_on_reset_password_token", unique: true
   end
 
 end
